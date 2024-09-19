9ae1743d149337f7b163f11058624afae2b846f6
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-19 10:42:10 +0200

    file committed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
       xl: '1440px',
     },
      
-    extend: {},
+    extend: {
+      xtend: {
+        boxShadow: {
+          'custom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        }
+      }
+    },
   },
   plugins: [],
 }
